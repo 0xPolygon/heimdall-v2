9146c333ca14fc85c3a5db13faa8595d2a85e33c
Vaibhav Jindal <vaibhavjindal29@gmail.com>
2024-03-01 19:05:50 +0530

    Add:sideTxConfigurator

--- x/types/module/sideTxConfigurator.go
+++ x/types/module/sideTxConfigurator.go
@@ -3,10 +3,8 @@ package module
 import (
 	"fmt"
 
-	"github.com/cosmos/gogoproto/grpc"
-
 	"github.com/0xPolygon/heimdall-v2/x/types"
-	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Configurator provides the hooks to allow modules to configure and register
@@ -17,6 +15,14 @@ type SideTxConfigurator interface {
 	RegisterSideHandler(msgURL string, handler types.SideTxHandler) error
 
 	RegisterPostHandler(msgURL string, handler types.PostTxHandler) error
+
+	SideHandler(msg sdk.Msg) types.SideTxHandler
+
+	PostHandler(msg sdk.Msg) types.PostTxHandler
+
+	SideHandlerByTypeURL(typeURL string) types.SideTxHandler
+
+	PostHandlerByTypeURL(typeURL string) types.PostTxHandler
 }
 
 type sideTxConfigurator struct {
@@ -28,7 +34,7 @@ type sideTxConfigurator struct {
 }
 
 // NewConfigurator returns a new Configurator instance
-func NewConfigurator(cdc codec.Codec, msgServer, queryServer grpc.Server) SideTxConfigurator {
+func NewConfigurator() SideTxConfigurator {
 	return &sideTxConfigurator{
 		sideHandlers: make(map[string]types.SideTxHandler),
 		postHandlers: make(map[string]types.PostTxHandler),
@@ -37,7 +43,6 @@ func NewConfigurator(cdc codec.Codec, msgServer, queryServer grpc.Server) SideTx
 
 // RegisterMigration implements the Configurator.RegisterMigration method
 func (c *sideTxConfigurator) RegisterSideHandler(msgURL string, handler types.SideTxHandler) error {
-
 	if c.sideHandlers[msgURL] == nil {
 		c.sideHandlers[msgURL] = handler
 		return nil
@@ -55,4 +60,26 @@ func (c *sideTxConfigurator) RegisterPostHandler(msgURL string, handler types.Po
 	}
 
 	return fmt.Errorf("PostHandler corresponding to the following msg %s already exist", msgURL)
+}
+
+// Handler returns the MsgServiceHandler for a given msg or nil if not found.
+func (c *sideTxConfigurator) SideHandler(msg sdk.Msg) types.SideTxHandler {
+	return c.sideHandlers[sdk.MsgTypeURL(msg)]
+}
+
+// Handler returns the MsgServiceHandler for a given msg or nil if not found.
+func (c *sideTxConfigurator) PostHandler(msg sdk.Msg) types.PostTxHandler {
+	return c.postHandlers[sdk.MsgTypeURL(msg)]
+}
+
+// SideHandlerByTypeURL returns the SideTxHandler for a given query route path or nil
+// if not found.
+func (c *sideTxConfigurator) SideHandlerByTypeURL(typeURL string) types.SideTxHandler {
+	return c.sideHandlers[typeURL]
+}
+
+// PostHandlerByTypeURL returns the PostTxHandler for a given query route path or nil
+// if not found.
+func (c *sideTxConfigurator) PostHandlerByTypeURL(typeURL string) types.PostTxHandler {
+	return c.postHandlers[typeURL]
 }
