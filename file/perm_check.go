bb852747c35832f5ed3df002e0eec9629448e39c
Vaibhav Jindal <vaibhavj@Polygon-L541PJQV59.local>
2024-04-22 20:38:43 +0530

    Added the extra condition in error check

--- file/perm_check.go
+++ file/perm_check.go
@@ -2,6 +2,7 @@ package file
 
 import (
 	"errors"
+	"fmt"
 	"os"
 
 	types "github.com/0xPolygon/heimdall-v2/types/error"
@@ -12,8 +13,18 @@ import (
 func PermCheck(filePath string, validPerm os.FileMode) error {
 	// get path to keystore files
 	f, err := os.Stat(filePath)
-	if err != nil && !errors.Is(err, os.ErrExist) {
-		return types.InvalidPermissionsError{File: filePath, Perm: validPerm, Err: err}
+
+	if err != nil {
+
+		if errors.Is(err, os.ErrNotExist) {
+			return errors.New(fmt.Sprintf("no file exist corresponding to this path: %s", filePath))
+
+		}
+
+		if !errors.Is(err, os.ErrExist) {
+			return types.InvalidPermissionsError{File: filePath, Perm: validPerm, Err: err}
+		}
+
 	}
 
 	filePerm := f.Mode()
