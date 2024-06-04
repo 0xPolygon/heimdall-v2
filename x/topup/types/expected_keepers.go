7a913d0753b883d1925c1d8e11a6b09666829282
marcello33 <marcelloardizzone@hotmail.it>
2024-06-04 14:18:08 +0200

    chg: use only subset of methods for bankKeeper

--- x/topup/types/expected_keepers.go
+++ x/topup/types/expected_keepers.go
@@ -1,10 +1,16 @@
 package types
 
 import (
-	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// BankKeeper import the cosmos-sdk/x/bank keeper
+// BankKeeper defines the contract used by x/topup module
 type BankKeeper interface {
-	bankkeeper.Keeper
+	IsSendEnabledDenom(ctx context.Context, denom string) bool
+	SpendableCoin(ctx context.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	BurnCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoins(ctx context.Context, fromAddr, toAddr sdk.AccAddress, amt sdk.Coins) error
 }
