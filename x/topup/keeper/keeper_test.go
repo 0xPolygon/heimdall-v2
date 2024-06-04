1ccaff1750427936061fd8e7b3ee15711c5aef88
marcello33 <marcelloardizzone@hotmail.it>
2024-06-04 14:21:34 +0200

    chg: move some functions to related test package

--- x/topup/keeper/keeper_test.go
+++ x/topup/keeper/keeper_test.go
@@ -1,8 +1,6 @@
 package keeper_test
 
 import (
-	mod "github.com/0xPolygon/heimdall-v2/module"
-	"github.com/cosmos/cosmos-sdk/codec/address"
 	"math/big"
 	"math/rand"
 	"strconv"
@@ -13,14 +11,17 @@ import (
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	cmttime "github.com/cometbft/cometbft/types/time"
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec/address"
 	"github.com/cosmos/cosmos-sdk/runtime"
 	cosmostestutil "github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
 
+	mod "github.com/0xPolygon/heimdall-v2/module"
 	"github.com/0xPolygon/heimdall-v2/types"
 	topupKeeper "github.com/0xPolygon/heimdall-v2/x/topup/keeper"
 	"github.com/0xPolygon/heimdall-v2/x/topup/testutil"
@@ -162,6 +163,35 @@ func (suite *KeeperTestSuite) TestDividendAccountTree() {
 	require.NoError(err)
 	*/
 
-	leafHash := testutil.CalculateDividendAccountHash(divAccounts[0])
+	leafHash := CalculateDividendAccountHash(divAccounts[0])
 	require.NotNil(leafHash)
+}
+
+// CalculateDividendAccountHash is a helper function to hash the values of a DividendAccount
+func CalculateDividendAccountHash(da types.DividendAccount) []byte {
+	fee, _ := big.NewInt(0).SetString(da.FeeAmount, 10)
+	divAccountHash := crypto.Keccak256(AppendBytes32([]byte(da.User), fee.Bytes()))
+
+	return divAccountHash
+}
+
+func AppendBytes32(data ...[]byte) []byte {
+	var result []byte
+
+	for _, v := range data {
+		paddedV := convertTo32(v)
+		result = append(result, paddedV[:]...)
+	}
+
+	return result
+}
+
+func convertTo32(input []byte) (output [32]byte) {
+	l := len(input)
+	if l > 32 || l == 0 {
+		return
+	}
+	copy(output[32-l:], input[:])
+
+	return output
 }
