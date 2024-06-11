9b2e45dd537a3ea9a5a9a569c7e3da6a33db4eed
Vaibhav Jindal <vaibhavj@Polygon-L541PJQV59.local>
2024-06-11 16:18:25 +0530

    Added the max condition

--- helper/backoff.go
+++ helper/backoff.go
@@ -7,6 +7,11 @@ import (
 // ExponentialBackoff performs exponential backoff attempts on a given action
 func ExponentialBackoff(action func() error, max uint, wait time.Duration) error {
 	var err error
+
+	if max > 10 {
+		max = 10
+	}
+
 	for i := uint(0); i < max; i++ {
 		if err = action(); err == nil {
 			break
