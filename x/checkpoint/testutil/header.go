89b9c4d8d0d8155e8a3d04484333e5715b58e359
Vaibhav Jindal <vaibhavj@Polygon-L541PJQV59.local>
2024-06-20 03:01:13 +0530

    addressed the comments

--- x/checkpoint/testutil/header.go
+++ x/checkpoint/testutil/header.go
@@ -9,8 +9,8 @@ import (
 	"github.com/0xPolygon/heimdall-v2/x/checkpoint/types"
 )
 
-// GenRandCheckpoint return headers
-func GenRandCheckpoint(start uint64, headerSize uint64, maxCheckpointLenght uint64) (headerBlock types.Checkpoint, err error) {
+// GenRandCheckpoint returns a random checkpoint header
+func GenRandCheckpoint(start uint64, headerSize uint64) (headerBlock types.Checkpoint) {
 	end := start + headerSize
 	borChainID := "1234"
 	rootHash := hmTypes.HexToHeimdallHash("123")
@@ -24,5 +24,5 @@ func GenRandCheckpoint(start uint64, headerSize uint64, maxCheckpointLenght uint
 		borChainID,
 		uint64(time.Now().UTC().Unix()))
 
-	return headerBlock, nil
+	return headerBlock
 }
