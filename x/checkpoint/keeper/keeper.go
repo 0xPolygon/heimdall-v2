df42f219d67aee87ee01ce9e96e90e7aa5e03578
Vaibhav Jindal <vaibhavj@Polygon-L541PJQV59.local>
2024-06-23 19:41:18 +0530

    Replaced the direct access to store with collections

--- x/checkpoint/keeper/keeper.go
+++ x/checkpoint/keeper/keeper.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 
+	"cosmossdk.io/collections"
 	storetypes "cosmossdk.io/core/store"
 	"cosmossdk.io/log"
 
@@ -17,13 +18,23 @@ import (
 
 // Keeper of the x/checkpoint store
 type Keeper struct {
-	storeService       storetypes.KVStoreService
-	cdc                codec.BinaryCodec
+	storeService storetypes.KVStoreService
+	cdc          codec.BinaryCodec
+	schema       collections.Schema
+
 	authority          string
 	sk                 stakeKeeper.Keeper
 	ck                 cmKeeper.Keeper
 	moduleCommunicator types.ModuleCommunicator
 	IContractCaller    helper.IContractCaller
+
+	checkpoint         collections.Map[uint64, types.Checkpoint]
+	bufferedCheckpoint collections.Item[*types.Checkpoint]
+	params             collections.Item[types.Params]
+	lastNoAck          collections.Item[uint64]
+	ackCount           collections.Item[uint64]
+
+	sequences collections.Map[string, bool]
 }
 
 // NewKeeper creates a new checkpoint Keeper instance
@@ -57,26 +68,15 @@ func (k Keeper) Logger(ctx context.Context) log.Logger {
 // SetParams sets the x/checkpoint module parameters.
 // CONTRACT: This method performs no validation of the parameters.
 func (k Keeper) SetParams(ctx context.Context, params types.Params) error {
-	store := k.storeService.OpenKVStore(ctx)
-	bz, err := k.cdc.Marshal(&params)
-	if err != nil {
-		return err
-	}
-	return store.Set(types.ParamsKey, bz)
+	return k.params.Set(ctx, params)
 }
 
 // GetParams gets the x/checkpoint module parameters.
 func (k Keeper) GetParams(ctx context.Context) (params types.Params, err error) {
-	store := k.storeService.OpenKVStore(ctx)
-	bz, err := store.Get(types.ParamsKey)
+	params, err = k.params.Get(ctx)
 	if err != nil {
 		return params, err
 	}
 
-	if bz == nil {
-		return params, nil
-	}
-
-	err = k.cdc.Unmarshal(bz, &params)
 	return params, err
 }
