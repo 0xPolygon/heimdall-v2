c316d0346bdfc19702b89c1e6e8d08b9c109df82
Pratik Patil <pratikspatil024@gmail.com>
2024-07-30 11:32:41 +0530

    bridge: added bridge/broadcaster

---
+++ bridge/setu/broadcaster/broadcaster.go
@@ -0,0 +1,150 @@
+package broadcaster
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"cosmossdk.io/log"
+	"github.com/0xPolygon/heimdall-v2/bridge/setu/util"
+	"github.com/0xPolygon/heimdall-v2/helper"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// TxBroadcaster is used to broadcast transaction to each chain
+type TxBroadcaster struct {
+	logger log.Logger
+
+	CliCtx client.Context
+
+	heimdallMutex sync.Mutex
+	maticMutex    sync.Mutex
+
+	lastSeqNo uint64
+	accNum    uint64
+}
+
+// NewTxBroadcaster creates a new instance of TxBroadcaster
+func NewTxBroadcaster(cdc codec.Codec) *TxBroadcaster {
+	cliCtx := client.Context{}.WithCodec(cdc)
+	cliCtx.BroadcastMode = flags.BroadcastSync
+
+	// current address
+	address := helper.GetAddress()
+
+	var account sdk.AccountI
+
+	account, err := util.GetAccount(cliCtx, string(address[:]))
+	if err != nil {
+		panic("Error connecting to rest-server, please start server before bridge.")
+	}
+
+	return &TxBroadcaster{
+		logger:    util.Logger(context.Background()).With("module", "txBroadcaster"),
+		CliCtx:    cliCtx,
+		lastSeqNo: account.GetSequence(),
+		accNum:    account.GetAccountNumber(),
+	}
+}
+
+// BroadcastToHeimdall broadcast to heimdall
+func (tb *TxBroadcaster) BroadcastToHeimdall(msg sdk.Msg, event interface{}) error {
+	tb.heimdallMutex.Lock()
+	defer tb.heimdallMutex.Unlock()
+	defer util.LogElapsedTimeForStateSyncedEvent(event, "BroadcastToHeimdall", time.Now())
+
+	// TODO HV2 - get help from informal team on
+	// 1. NewTxBuilderFromCLI (unavailable in cosmos-sdk)
+	// 2. BuildAndBroadcastMsgs (removed/unavailable in helper)
+	/*
+		// tx encoder
+		txEncoder := authlegacytx.DefaultTxEncoder(tb.CliCtx.LegacyAmino)
+		// chain id
+		chainID := helper.GetGenesisDoc().ChainID
+
+		// get TxBuilder
+		txBldr := authTypes.NewTxBuilderFromCLI().
+			WithTxEncoder(txEncoder).
+			WithAccountNumber(tb.accNum).
+			WithSequence(tb.lastSeqNo).
+			WithChainID(chainID)
+
+		txResponse, err := helper.BuildAndBroadcastMsgs(tb.CliCtx, txBldr, []sdk.Msg{msg})
+		if err != nil {
+			tb.logger.Error("Error while broadcasting the heimdall transaction", "error", err)
+
+			// current address
+			address := helper.GetAddress()
+
+			// fetch from APIs
+			account, errAcc := util.GetAccount(tb.CliCtx, string(address[:]))
+			if errAcc != nil {
+				tb.logger.Error("Error fetching account from rest-api", "url", helper.GetHeimdallServerEndpoint(fmt.Sprintf(util.AccountDetailsURL, helper.GetAddress())))
+				return errAcc
+			}
+
+			// update seqNo for safety
+			tb.lastSeqNo = account.GetSequence()
+
+			return err
+		}
+
+		txHash := txResponse.TxHash
+
+		tb.logger.Info("Tx sent on heimdall", "txHash", txHash, "accSeq", tb.lastSeqNo, "accNum", tb.accNum)
+		tb.logger.Debug("Tx successful on heimdall", "txResponse", txResponse)
+		// increment account sequence
+		tb.lastSeqNo += 1
+	*/
+
+	return nil
+}
+
+// BroadcastToMatic broadcast to matic
+func (tb *TxBroadcaster) BroadcastToMatic(msg ethereum.CallMsg) error {
+	tb.maticMutex.Lock()
+	defer tb.maticMutex.Unlock()
+
+	// get matic client
+	maticClient := helper.GetMaticClient()
+
+	// get auth
+	auth, err := helper.GenerateAuthObj(maticClient, *msg.To, msg.Data)
+
+	if err != nil {
+		tb.logger.Error("Error generating auth object", "error", err)
+		return err
+	}
+
+	// Create the transaction, sign it and schedule it for execution
+	rawTx := types.NewTransaction(auth.Nonce.Uint64(), *msg.To, msg.Value, auth.GasLimit, auth.GasPrice, msg.Data)
+
+	// signer
+	signedTx, err := auth.Signer(auth.From, rawTx)
+	if err != nil {
+		tb.logger.Error("Error signing the transaction", "error", err)
+		return err
+	}
+
+	tb.logger.Info("Sending transaction to bor", "txHash", signedTx.Hash())
+
+	// create a context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), helper.GetConfig().BorRPCTimeout)
+	defer cancel()
+
+	// broadcast transaction
+	if err := maticClient.SendTransaction(ctx, signedTx); err != nil {
+		tb.logger.Error("Error while broadcasting the transaction to maticchain", "error", err)
+		return err
+	}
+
+	return nil
+}
+
+// BroadcastToRootchain broadcast to rootchain
+func (tb *TxBroadcaster) BroadcastToRootchain() {}
---
+++ bridge/setu/broadcaster/broadcaster_test.go
@@ -0,0 +1,66 @@
+package broadcaster
+
+import (
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/0xPolygon/heimdall-v2/helper"
+	hmTypes "github.com/0xPolygon/heimdall-v2/types"
+	checkpointTypes "github.com/0xPolygon/heimdall-v2/x/checkpoint/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+// Parallel test - to check BroadcastToHeimdall synchronisation
+func TestBroadcastToHeimdall(t *testing.T) {
+	t.Parallel()
+
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	cryptocodec.RegisterInterfaces(interfaceRegistry)
+	cdc := codec.NewProtoCodec(interfaceRegistry)
+
+	// cli context
+	CometBFTNode := "tcp://localhost:26657"
+	viper.Set(helper.CometBFTNodeFlag, CometBFTNode)
+	viper.Set("log_level", "info")
+	// cliCtx := cliContext.NewCLIContext().WithCodec(cdc)
+	// cliCtx.BroadcastMode = client.BroadcastSync
+	// cliCtx.TrustNode = true
+
+	helper.InitHeimdallConfig(os.ExpandEnv("$HOME/.heimdalld"))
+
+	_txBroadcaster := NewTxBroadcaster(cdc)
+
+	testData := []checkpointTypes.MsgCheckpoint{
+		{Proposer: string(helper.GetAddress()[:]), StartBlock: 0, EndBlock: 63, RootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}, AccountRootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}},
+		{Proposer: string(helper.GetAddress()[:]), StartBlock: 64, EndBlock: 1024, RootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}, AccountRootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}},
+		{Proposer: string(helper.GetAddress()[:]), StartBlock: 1025, EndBlock: 2048, RootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}, AccountRootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}},
+		{Proposer: string(helper.GetAddress()[:]), StartBlock: 2049, EndBlock: 3124, RootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}, AccountRootHash: hmTypes.HeimdallHash{Hash: []byte("0x5bd83f679c8ce7c48d6fa52ce41532fcacfbbd99d5dab415585f397bf44a0b6e")}},
+	}
+
+	for index, test := range testData { //nolint
+		index := index
+		test := test
+
+		t.Run(strconv.Itoa(index), func(t *testing.T) {
+			t.Parallel()
+
+			// create and send checkpoint message
+			msg := checkpointTypes.NewMsgCheckpointBlock(
+				test.Proposer,
+				test.StartBlock,
+				test.EndBlock,
+				test.RootHash,
+				test.AccountRootHash,
+				test.BorChainID,
+			)
+
+			err := _txBroadcaster.BroadcastToHeimdall(&msg, nil)
+			assert.Empty(t, err, "Error broadcasting tx to heimdall", err)
+		})
+	}
+}
