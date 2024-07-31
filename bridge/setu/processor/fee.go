03d16b72bf72921215b63953f4cdb207219a34dc
Pratik Patil <pratikspatil024@gmail.com>
2024-07-31 13:52:45 +0530

    bridge: added bridge/processor/fee.go

---
+++ bridge/setu/processor/fee.go
@@ -0,0 +1,87 @@
+package processor
+
+import (
+	math "cosmossdk.io/math"
+	"github.com/0xPolygon/heimdall-v2/bridge/setu/util"
+	"github.com/0xPolygon/heimdall-v2/contracts/stakinginfo"
+	"github.com/0xPolygon/heimdall-v2/helper"
+	hmTypes "github.com/0xPolygon/heimdall-v2/types"
+	topupTypes "github.com/0xPolygon/heimdall-v2/x/topup/types"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/core/types"
+	jsoniter "github.com/json-iterator/go"
+)
+
+// FeeProcessor - process fee related events
+type FeeProcessor struct {
+	BaseProcessor
+	stakingInfoAbi *abi.ABI
+}
+
+// NewFeeProcessor - add  abi to clerk processor
+func NewFeeProcessor(stakingInfoAbi *abi.ABI) *FeeProcessor {
+	return &FeeProcessor{
+		stakingInfoAbi: stakingInfoAbi,
+	}
+}
+
+// Start starts new block subscription
+func (fp *FeeProcessor) Start() error {
+	fp.Logger.Info("Starting")
+	return nil
+}
+
+// RegisterTasks - Registers clerk related tasks with machinery
+func (fp *FeeProcessor) RegisterTasks() {
+	fp.Logger.Info("Registering fee related tasks")
+
+	if err := fp.queueConnector.Server.RegisterTask("sendTopUpFeeToHeimdall", fp.sendTopUpFeeToHeimdall); err != nil {
+		fp.Logger.Error("RegisterTasks | sendTopUpFeeToHeimdall", "error", err)
+	}
+}
+
+// processTopupFeeEvent - processes topup fee event
+func (fp *FeeProcessor) sendTopUpFeeToHeimdall(eventName string, logBytes string) error {
+	var vLog = types.Log{}
+	if err := jsoniter.ConfigFastest.Unmarshal([]byte(logBytes), &vLog); err != nil {
+		fp.Logger.Error("Error while unmarshalling event from rootchain", "error", err)
+		return err
+	}
+
+	event := new(stakinginfo.StakinginfoTopUpFee)
+	if err := helper.UnpackLog(fp.stakingInfoAbi, event, eventName, &vLog); err != nil {
+		fp.Logger.Error("Error while parsing event", "name", eventName, "error", err)
+	} else {
+		if isOld, _ := fp.isOldTx(fp.cliCtx, vLog.TxHash.String(), uint64(vLog.Index), util.TopupEvent, event); isOld {
+			fp.Logger.Info("Ignoring task to send topup to heimdall as already processed",
+				"event", eventName,
+				"user", event.User,
+				"Fee", event.Fee,
+				"txHash", vLog.TxHash.String(),
+				"logIndex", uint64(vLog.Index),
+				"blockNumber", vLog.BlockNumber,
+			)
+			return nil
+		}
+
+		fp.Logger.Info("✅ sending topup to heimdall",
+			"event", eventName,
+			"user", event.User,
+			"Fee", event.Fee,
+			"txHash", vLog.TxHash.String(),
+			"logIndex", uint64(vLog.Index),
+			"blockNumber", vLog.BlockNumber,
+		)
+
+		// create msg checkpoint ack message
+		msg := topupTypes.NewMsgTopupTx(helper.GetFromAddress(fp.cliCtx), event.User.String(), math.NewIntFromBigInt(event.Fee), hmTypes.TxHash{Hash: vLog.TxHash.Bytes()}, uint64(vLog.Index), vLog.BlockNumber)
+
+		// return broadcast to heimdall
+		if err = fp.txBroadcaster.BroadcastToHeimdall(msg, event); err != nil {
+			fp.Logger.Error("Error while broadcasting TopupFee msg to heimdall", "msg", msg, "error", err)
+			return err
+		}
+	}
+
+	return nil
+}
