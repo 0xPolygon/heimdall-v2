18e7b7cfa032b1a9ce5fa42164312d32cac0b348
Pratik Patil <pratikspatil024@gmail.com>
2024-08-08 14:53:19 +0530

    bridge: added bridge/cmd/reset.go

---
+++ bridge/cmd/reset.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"os"
+	"path"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/spf13/cobra"
+)
+
+// resetCmd resets the bridge server data
+var resetCmd = &cobra.Command{
+	Use:   "unsafe-reset-all",
+	Short: "Reset bridge server data",
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		serverCtx := server.GetServerContextFromCmd(cmd)
+
+		dbLocation := serverCtx.Viper.GetString(bridgeDBFlag)
+		dir, err := os.ReadDir(dbLocation)
+		if err != nil {
+			return err
+		}
+
+		for _, d := range dir {
+			os.RemoveAll(path.Join([]string{dbLocation, d.Name()}...))
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resetCmd)
+}
