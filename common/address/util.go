92fde2d18d1561317a37cd9df32e593ee836d487
marcello33 <marcelloardizzone@hotmail.it>
2024-10-14 17:31:52 +0200

    chg: optimize formatAddress

--- common/address/util.go
+++ common/address/util.go
@@ -6,13 +6,5 @@ import "strings"
 func FormatAddress(hexAddr string) string {
 	hexAddr = strings.ToLower(hexAddr)
 
-	if !has0xPrefix(hexAddr) {
-		hexAddr = "0x" + hexAddr
-	}
-	return hexAddr
-}
-
-// has0xPrefix validates str begins with '0x' or '0X'.
-func has0xPrefix(str string) bool {
-	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
+	return "0x" + strings.TrimPrefix(hexAddr, "0x")
 }
