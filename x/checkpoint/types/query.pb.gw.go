3e35c0d8a9a99791670711248158a82f2bcee389
Angel Valkov <avalkov@polygon.technology>
2025-01-16 15:50:22 +0200

    Improve Signatures API

--- x/checkpoint/types/query.pb.gw.go
+++ x/checkpoint/types/query.pb.gw.go
@@ -285,19 +285,26 @@ func local_request_Query_GetCheckpointList_0(ctx context.Context, marshaler runt
 
 }
 
-var (
-	filter_Query_GetCheckpointSignatures_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_Query_GetCheckpointSignatures_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryCheckpointSignaturesRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["tx_hash"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "tx_hash")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_GetCheckpointSignatures_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.TxHash, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "tx_hash", err)
 	}
 
 	msg, err := client.GetCheckpointSignatures(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -309,11 +316,22 @@ func local_request_Query_GetCheckpointSignatures_0(ctx context.Context, marshale
 	var protoReq QueryCheckpointSignaturesRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["tx_hash"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "tx_hash")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_GetCheckpointSignatures_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.TxHash, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "tx_hash", err)
 	}
 
 	msg, err := server.GetCheckpointSignatures(ctx, &protoReq)
@@ -962,7 +980,7 @@ var (
 
 	pattern_Query_GetCheckpointList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"checkpoints", "list"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_GetCheckpointSignatures_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"checkpoint", "signatures"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_GetCheckpointSignatures_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"checkpoint", "signatures", "tx_hash"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_GetCheckpoint_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"checkpoints", "number"}, "", runtime.AssumeColonVerbOpt(false)))
 )
