64e91598955849486334856c8520096fd51e97ad
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-03-04 11:23:09 -0300

    including milestone events

--- x/milestone/types/events.go
+++ x/milestone/types/events.go
@@ -1,5 +1,12 @@
 package types
 
+import (
+	"encoding/hex"
+	"strconv"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // Milestone tags
 var (
 	AttributeKeyProposer      = "proposer"
@@ -12,3 +19,19 @@ var (
 	EventTypeMilestone        = "milestone"
 	EventTypeMilestoneTimeout = "milestone-timeout"
 )
+
+// NewMilestoneEvent construct a new coin minted sdk.Event
+func NewMilestoneEvent(milestone Milestone, milestoneNumber uint64) sdk.Event {
+	return sdk.NewEvent(
+		EventTypeMilestone,
+		sdk.NewAttribute("hash", string(milestone.Hash)),
+		sdk.NewAttribute("proposer", milestone.Proposer),
+		sdk.NewAttribute("hash", hex.EncodeToString(milestone.Hash)),
+		sdk.NewAttribute("start_block", strconv.FormatUint(milestone.StartBlock, 10)),
+		sdk.NewAttribute("end_block", strconv.FormatUint(milestone.EndBlock, 10)),
+		sdk.NewAttribute("bor_chain_id", milestone.BorChainId),
+		sdk.NewAttribute("milestone_id", milestone.MilestoneId),
+		sdk.NewAttribute("timestamp", strconv.FormatUint(milestone.Timestamp, 10)),
+		sdk.NewAttribute("number", strconv.FormatUint(milestoneNumber, 10)),
+	)
+}
