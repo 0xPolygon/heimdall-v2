96909c0aa258da4c4c56b3f8ce406a1970a38480
Arya Lanjewar <lanjewararya@iitbhilai.ac.in>
2025-03-12 16:14:15 +0530

    DefaultGenesis State initialised for stake module with correct validator profile

--- x/stake/types/genesis.go
+++ x/stake/types/genesis.go
@@ -2,6 +2,8 @@ package types
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 )
@@ -18,9 +20,74 @@ func NewGenesisState(validators []*Validator,
 	}
 }
 
+type PublicKey struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type PrivKey struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type ValidatorKey struct {
+	Address string    `json:"address"`
+	PubKey  PublicKey `json:"pub_key"`
+	PrivKey PrivKey   `json:"priv_key"`
+}
+
 // DefaultGenesisState gets the raw genesis raw message for testing
 func DefaultGenesisState() *GenesisState {
-	return &GenesisState{}
+	content, err := os.ReadFile("/Users/alanjewar/var/lib/heimdall/config/priv_validator_key.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var validatorKey ValidatorKey
+	err = json.Unmarshal(content, &validatorKey)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	signer := "0x" + validatorKey.Address
+	pubKeyValue := validatorKey.PubKey.Value
+
+	fmt.Println("Signer Address: ", signer)
+	fmt.Println("Public Key: ", pubKeyValue)
+
+	return &GenesisState{
+		Validators: []*Validator{
+			{
+				ValId:            1,
+				StartEpoch:       0,
+				EndEpoch:         1000000,
+				Nonce:            0,
+				VotingPower:      1000,
+				PubKey:           []byte(pubKeyValue),
+				Signer:           signer,
+				LastUpdated:      "hello",
+				Jailed:           false,
+				ProposerPriority: 0,
+			},
+		},
+		CurrentValidatorSet: ValidatorSet{
+			Validators: []*Validator{
+				{
+					ValId:            1,
+					StartEpoch:       0,
+					EndEpoch:         1000000,
+					Nonce:            0,
+					VotingPower:      1000,
+					PubKey:           []byte(pubKeyValue),
+					Signer:           signer,
+					LastUpdated:      "hello",
+					Jailed:           false,
+					ProposerPriority: 0,
+				},
+			},
+		},
+		StakingSequences: []string{"initial_stake"},
+	}
 }
 
 // GetGenesisStateFromAppState returns x/stake GenesisState given raw application
