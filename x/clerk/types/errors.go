6cde176abd9c44ac0154b49c72a309a1514d244d
marcello33 <marcelloardizzone@hotmail.it>
2025-05-12 18:26:53 +0200

    fix error code

--- x/clerk/types/errors.go
+++ x/clerk/types/errors.go
@@ -7,5 +7,5 @@ import (
 var (
 	ErrEventRecordAlreadySynced = errors.Register(ModuleName, 5400, "Event record already synced")
 	ErrSizeExceed               = errors.Register(ModuleName, 5401, "Data size exceed")
-	ErrInvalidTxHash            = errors.Register(ModuleName, 5403, "Invalid tx hash")
+	ErrInvalidTxHash            = errors.Register(ModuleName, 5402, "Invalid tx hash")
 )
