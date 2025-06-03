889bcc5b956d280a8eefc6e0ecee103e793ad3e0
Angel Valkov <avalkov@polygon.technology>
2025-06-03 14:54:22 +0300

    Added logging

--- x/bor/keeper/side_msg_server.go
+++ x/bor/keeper/side_msg_server.go
@@ -309,6 +309,7 @@ func (s sideMsgServer) PostHandleMsgBackfillSpans(ctx sdk.Context, msgI sdk.Msg,
 	borSpans := types.GenerateBorCommittedSpans(latestMilestone.EndBlock, &latestSpan)
 	spansOverlap := 0
 	for i := range borSpans {
+		logger.Error(fmt.Sprintf("bor span %d: %+v", i, borSpans[i]))
 		if _, err := s.k.GetSpan(ctx, borSpans[i].Id); err != nil {
 			spansOverlap++
 		}
