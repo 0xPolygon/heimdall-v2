51457f545cd455a17b2b1241fbc358b5fbf523bd
kamuikatsurgi <shahkrishang11@gmail.com>
2025-07-22 14:22:19 +0530

    init: metrics package

--- cmd/heimdalld/main.go
+++ cmd/heimdalld/main.go
@@ -8,11 +8,12 @@ import (
 
 	"github.com/0xPolygon/heimdall-v2/app"
 	heimdalld "github.com/0xPolygon/heimdall-v2/cmd/heimdalld/cmd"
+	"github.com/0xPolygon/heimdall-v2/metrics"
 	"github.com/0xPolygon/heimdall-v2/version"
 )
 
 func main() {
-	version.UpdateHeimdalldInfo()
+	metrics.UpdateHeimdallV2Info(version.Version, version.Commit)
 	rootCmd := heimdalld.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, "HD", app.DefaultNodeHome); err != nil {
 		_, _ = fmt.Fprintln(rootCmd.OutOrStderr(), err)
---
+++ metrics/common.go
@@ -0,0 +1,6 @@
+package metrics
+
+const (
+	// Namespace is the prefix for all metrics.
+	Namespace = "heimdallv2"
+)
