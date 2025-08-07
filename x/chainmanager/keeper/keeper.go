0d9255eed5b1b72a392d55ea44da557cc051908d
Raneet Debnath <raneetdebnath10@gmail.com>
2025-08-07 10:33:47 +0530

    app,x/chainmanager: rm unused key + minor nit

--- x/chainmanager/keeper/keeper.go
+++ x/chainmanager/keeper/keeper.go
@@ -19,11 +19,10 @@ import (
 
 // Keeper stores all chainmanager related data
 type Keeper struct {
-	cdc                codec.BinaryCodec
-	storeService       store.KVStoreService
-	Schema             collections.Schema
-	params             collections.Item[types.Params]
-	initialChainHeight collections.Item[int64]
+	cdc          codec.BinaryCodec
+	storeService store.KVStoreService
+	Schema       collections.Schema
+	params       collections.Item[types.Params]
 	// The address capable of executing a `MsgUpdateParams` message.
 	// This should be the x/gov module account.
 	authority string
@@ -48,11 +47,10 @@ func NewKeeper(
 	sb := collections.NewSchemaBuilder(storeService)
 
 	k := Keeper{
-		cdc:                cdc,
-		storeService:       storeService,
-		params:             collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc)),
-		initialChainHeight: collections.NewItem(sb, types.InitialChainHeightKey, "initial_chain_height", collections.Int64Value),
-		authority:          authority,
+		cdc:          cdc,
+		storeService: storeService,
+		params:       collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc)),
+		authority:    authority,
 	}
 
 	schema, err := sb.Build()
