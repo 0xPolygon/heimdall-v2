6cdb60b052703745e7dde9fe0aa0f59f37574cee
Angel Valkov <avalkov@polygon.technology>
2025-11-05 16:19:06 +0200

    Fix build

--- x/bor/keeper/msg_server.go
+++ x/bor/keeper/msg_server.go
@@ -285,7 +285,6 @@ func (s msgServer) SetProducerDowntime(ctx context.Context, msg *types.MsgSetPro
 		return nil, fmt.Errorf("producer with address %s not found in the current validator set", msg.Producer)
 	}
 
-	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	candidates, err := s.CalculateProducerSet(ctx, helper.GetProducerSetLimit(sdkCtx))
 	if err != nil {
 		return nil, fmt.Errorf("failed to calculate producer set: %w", err)
