ebb98e9740b0feff3f40a76ffb4d48756c13777e
kamuikatsurgi <shahkrishang11@gmail.com>
2026-01-19 15:35:43 +0530

    helper(tx): add EIP-1559 dynamic gas pricing for L1 transactions

--- helper/tx.go
+++ helper/tx.go
@@ -21,77 +21,121 @@ import (
 )
 
 func GenerateAuthObj(client *ethclient.Client, address common.Address, data []byte) (auth *bind.TransactOpts, err error) {
-	// generate call msg
+	ctx := context.Background()
+
 	callMsg := ethereum.CallMsg{
 		To:   &address,
 		Data: data,
 	}
 
-	// get the private key
 	pkObject := GetPrivKey()
-
-	// create ecdsa private key
 	ecdsaPrivateKey, err := crypto.ToECDSA(pkObject[:])
 	if err != nil {
 		return
 	}
 
-	// from address
+	// Get the from address.
 	fromAddress := common.BytesToAddress(pkObject.PubKey().Address().Bytes())
-	// fetch gasPrice
-	gasPrice, err := client.SuggestGasPrice(context.Background())
+
+	// Get the configured gas fee cap and tip cap.
+	configGasFeeCap := GetConfig().MainChainGasFeeCap
+	if configGasFeeCap <= 0 {
+		configGasFeeCap = DefaultMainChainGasFeeCap
+	}
+
+	configGasTipCap := GetConfig().MainChainGasTipCap
+	if configGasTipCap <= 0 {
+		configGasTipCap = DefaultMainChainGasTipCap
+	}
+
+	// Get the latest block to retrieve baseFee.
+	latestBlock, err := client.BlockByNumber(ctx, nil)
+	if err != nil {
+		Logger.Error("unable to fetch latest block", "error", err)
+		return
+	}
+
+	baseFee := latestBlock.BaseFee()
+	if baseFee == nil {
+		err = errors.New("baseFee is nil, EIP-1559 not supported")
+		Logger.Error("EIP-1559 not supported on this chain", "error", err)
+		return
+	}
+
+	// Get suggested tip cap from the network.
+	suggestedTipCap, err := client.SuggestGasTipCap(ctx)
+	if err != nil {
+		Logger.Error("unable to fetch suggested gas tip cap", "error", err)
+		return
+	}
+
+	// Use the lower of suggested tip or configured tip cap to prioritize lower fees.
+	// The configured tip cap acts as a maximum we're willing to pay.
+	gasTipCap := suggestedTipCap
+	if gasTipCap.Cmp(big.NewInt(configGasTipCap)) > 0 {
+		gasTipCap = big.NewInt(configGasTipCap)
+	}
+
+	// Calculate gas fee cap: (baseFee * 2) + tipCap.
+	// The 2x multiplier provides buffer for baseFee fluctuations.
+	gasFeeCap := new(big.Int).Mul(baseFee, big.NewInt(2))
+	gasFeeCap.Add(gasFeeCap, gasTipCap)
+
+	// Cap the gas fee cap to configured maximum.
+	maxGasFeeCap := big.NewInt(configGasFeeCap)
+	if gasFeeCap.Cmp(maxGasFeeCap) > 0 {
+		Logger.Warn("calculated gas fee cap exceeds configured maximum, using configured maximum",
+			"calculated", gasFeeCap, "configured", maxGasFeeCap)
+		gasFeeCap = maxGasFeeCap
+	}
+
+	// Ensure tip cap doesn't exceed fee cap.
+	if gasTipCap.Cmp(gasFeeCap) > 0 {
+		gasTipCap = gasFeeCap
+	}
+
+	// Use PendingNonceAt to account for pending transactions in the mempool.
+	nonce, err := client.PendingNonceAt(ctx, fromAddress)
 	if err != nil {
 		return
 	}
 
-	mainChainMaxGasPrice := GetConfig().MainChainMaxGasPrice
-	// Check if configured or not, Use default in case of invalid value
-	if mainChainMaxGasPrice <= 0 {
-		mainChainMaxGasPrice = DefaultMainChainMaxGasPrice
-	}
-
-	if gasPrice.Cmp(big.NewInt(mainChainMaxGasPrice)) == 1 {
-		Logger.Error("gas price is more than max gas price", "gasprice", gasPrice)
-		err = fmt.Errorf("gas price is more than max_gas_price, gasprice = %v, maxGasPrice = %d", gasPrice, mainChainMaxGasPrice)
-
-		return
-	}
-
-	nonce, err := client.NonceAt(context.Background(), fromAddress, nil)
-	if err != nil {
-		return
-	}
-
-	// fetch gas limit
 	callMsg.From = fromAddress
-
-	gasLimit, err := client.EstimateGas(context.Background(), callMsg)
+	gasLimit, err := client.EstimateGas(ctx, callMsg)
 	if err != nil {
 		Logger.Error("unable to estimate gas", "error", err)
 		return
 	}
 
-	chainId, err := client.ChainID(context.Background())
+	chainId, err := client.ChainID(ctx)
 	if err != nil {
 		Logger.Error("unable to fetch ChainID", "error", err)
 		return
 	}
 
-	// create auth
 	auth, err = bind.NewKeyedTransactorWithChainID(ecdsaPrivateKey, chainId)
 	if err != nil {
 		Logger.Error("unable to create auth object", "error", err)
 		return
 	}
 
-	auth.GasPrice = gasPrice
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.GasLimit = gasLimit
+	auth.GasFeeCap = gasFeeCap
+	auth.GasTipCap = gasTipCap
+
+	Logger.Debug("created EIP-1559 transaction auth",
+		"nonce", nonce,
+		"gasLimit", gasLimit,
+		"gasFeeCap", gasFeeCap,
+		"gasTipCap", gasTipCap,
+		"baseFee", baseFee,
+	)
 
 	return
 }
 
-// SendCheckpoint sends checkpoint to rootChain contract
+// SendCheckpoint sends checkpoint to rootChain contract.
 func (c *ContractCaller) SendCheckpoint(signedData []byte, sigs [][3]*big.Int, rootChainAddress common.Address, rootChainInstance *rootchain.Rootchain) error {
 	data, err := c.RootChainABI.Pack("submitCheckpoint", signedData, sigs)
 	if err != nil {
